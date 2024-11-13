38f071f1cc3d3146c2a4261cf7d6bac644d53730
Maxson Almeida <maxsonferovante@gmail.com>
2024-11-12 22:04:31 -0300

    format files

--- src/main/java/com/maal/gupy/controllers/HealthCheck.java
+++ src/main/java/com/maal/gupy/controllers/HealthCheck.java
@@ -1,16 +1,19 @@
 package com.maal.gupy.controllers;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@RequestMapping("/health")
 public class HealthCheck {
 
     @GetMapping()
-    public String healthCheck() {
-        return new Date().toString();
+    public ResponseEntity<String> healthCheckResponseEntity() {
+        return ResponseEntity.ok("Health check OK! " + new Date());
     }
 }
