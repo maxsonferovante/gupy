fd2fce2801f3c65ca9d1a7ac60751196f06fcb91
Maxson Almeida <maxsonferovante@gmail.com>
2024-11-23 10:52:45 -0300

    create test unity job service - getAttJobs

---
+++ src/test/java/com/maal/gupy/services/JobServiceTest.java
@@ -0,0 +1,84 @@
+package com.maal.gupy.services;
+
+import com.maal.gupy.domain.job.Job;
+import com.maal.gupy.domain.job.JobRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class JobServiceTest {
+
+    @Mock
+    private JobRepository jobRepo;
+
+
+    @InjectMocks
+    private JobService jobService;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void createJob() {
+    }
+
+    @Test
+    void createBatchJob() {
+    }
+
+    @Test
+    void findJobsisRemoteWork() {
+    }
+
+    @Test
+    void findJobsByDescription() {
+    }
+
+    @Test
+    void findJobById() {
+    }
+
+    @Test
+    void findJobByIdentifier() {
+    }
+
+    @Test
+    void getAttlJobs() {
+
+        Pageable mockPageable = PageRequest.of(0, 10);
+
+        Page<Job> emptyPage = new PageImpl<>(Collections.emptyList(), mockPageable, 0);
+
+        when(this.jobRepo.findAll(mockPageable)).thenReturn(emptyPage);
+
+        Page<Job> result = this.jobService.getAttlJobs(mockPageable);
+
+        assertNotNull(result);
+        assertEquals(0  , result.getTotalElements());
+        verify(this.jobRepo).findAll(mockPageable);
+    }
+
+    @Test
+    @DisplayName("")
+    void deleteJobById() {
+
+    }
+}
